2abe4e36efd9c65d030a2c551c8ee62a5f4392bf
Thibault Durand <tibo.durand@gmail.com>
2020-12-05 08:10:08 -0300

    WIP: working on hooking image analysis on each frame

--- android/app/src/main/java/com/opendatacam/CameraActivity.java
+++ android/app/src/main/java/com/opendatacam/CameraActivity.java
@@ -1,6 +1,11 @@
 package com.opendatacam;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
 import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.os.Bundle;
 import android.util.Size;
 import android.view.LayoutInflater;
@@ -11,6 +16,8 @@ import androidx.annotation.NonNull;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -21,7 +28,13 @@ import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class CameraActivity extends Fragment {
@@ -37,6 +50,12 @@ public class CameraActivity extends Fragment {
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
+    private AtomicBoolean isDetectingOnCamera = new AtomicBoolean(false);
+
+    ExecutorService cameraExecutor;
+
+    private double threshold = 0.3, nms_threshold = 0.7;
+
     // Constructor
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,6 +65,8 @@ public class CameraActivity extends Fragment {
         previewView = view.findViewById(R.id.preview_view);
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(getActivity());
+
+        cameraExecutor = Executors.newSingleThreadExecutor();
 
         cameraProviderFuture.addListener(() -> {
             try {
@@ -69,9 +90,29 @@ public class CameraActivity extends Fragment {
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(480, 640))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        imageAnalysis.setAnalyzer(cameraExecutor, new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                int rotationDegrees = image.getImageInfo().getRotationDegrees();
+                // insert your code here.
+                System.out.println("imageAnalysis loop");
+                detectOnModel(image);
+            }
+        });
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll();
+
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, imageAnalysis, preview);
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
     }
 
@@ -80,5 +121,83 @@ public class CameraActivity extends Fragment {
         this.y = y;
         this.width = width;
         this.height = height;
+    }
+
+    private byte[] imagetToNV21(ImageProxy image) {
+        ImageProxy.PlaneProxy[] planes = image.getPlanes();
+        ImageProxy.PlaneProxy y = planes[0];
+        ImageProxy.PlaneProxy u = planes[1];
+        ImageProxy.PlaneProxy v = planes[2];
+        ByteBuffer yBuffer = y.getBuffer();
+        ByteBuffer uBuffer = u.getBuffer();
+        ByteBuffer vBuffer = v.getBuffer();
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        // U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        return nv21;
+    }
+
+    private Bitmap imageToBitmap(ImageProxy image) {
+        byte[] nv21 = imagetToNV21(image);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 100, out);
+        byte[] imageBytes = out.toByteArray();
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+
+    private void detectOnModel(ImageProxy image) {
+        /*
+        if (isDetectingOnCamera.get()) {
+            return;
+        }
+        isDetectingOnCamera.set(true);
+        */
+
+        final Bitmap bitmapsrc = imageToBitmap(image);
+
+        /*
+        if (cameraExecutor == null) {
+            isDetectingOnCamera.set(false);
+            return;
+        } */
+
+        cameraExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                width = bitmapsrc.getWidth();
+                height = bitmapsrc.getHeight();
+                Bitmap bitmap = Bitmap.createBitmap(bitmapsrc, 0, 0, width, height);
+                System.out.println("Detect loop");
+                //isDetectingOnCamera.set(false);
+                //detect(bitmap);
+            }
+        });
+    }
+
+    protected Box[] detect(Bitmap image) {
+        Box[] result = null;
+
+        System.out.println("detecting on frame using YOLOv4.detect()");
+
+        result = YOLOv4.detect(image, threshold, nms_threshold);
+
+        System.out.println("detecting on frame sucess return result");
+
+        //isDetectingOnCamera.set(false);
+
+        return result;
     }
 }
